e66c8fa9bd833c80c48f47f6e6d3d5094d4edf30
JuanR1207 <juanjose.2006rendon@gmail.com>
2026-02-17 09:22:02 -0500

    Clase 4 AGENDA ADSO v2

--- agenda-adso/src/App.jsx
+++ agenda-adso/src/App.jsx
@@ -1,9 +1,110 @@
+// import "./App.css"; // Importamos estilos de la app 
+
+
+// import ContactoCard from "src/Componentes/contactocard"; // Importamos el componente hijo 
+
+// export default function App() { 
+//   // Esta es nuestra "base de datos" inicial quemada en el código 
+//   const contactos = [ 
+//     { 
+//       id: 1, 
+//       nombre: "Carolina Pérez", 
+//       telefono: "300 123 4567", 
+//       correo: "carolina@sena.edu.co", 
+//       etiqueta: "Compañera", 
+//     }, 
+//     { 
+//       id: 2, 
+//       nombre: "Juan Díaz", 
+//       telefono: "301 987 6543", 
+//       correo: "juan@sena.edu.co", 
+//       etiqueta: "Instructor", 
+//     }, 
+//     {
+//       id: 3, 
+//       nombre: "Luisa Martínez", 
+//       telefono: "320 555 7788", 
+//       correo: "luisa@sena.edu.co", 
+//       etiqueta: "Cliente", 
+//     }, 
+//     {
+//       id: 4,
+//       nombre: "Karol Gómez",
+//       telefono: "322 444 5566",
+//       correo: "karolG@sena.edu.co",
+//       etiqueta: "Pareja",
+//     },
+//     {
+//       id: 5,
+//       nombre: "Ferney Alvarez",
+//       telefono: "321 333 2211",
+//       correo: "ferneyA@sena.edu.co",
+//       etiqueta: "Amigo",
+//     },
+//   ]; 
+//   return ( 
+//     <main className="app-container"> 
+//       <h1 className="app-title">Agenda ADSO 📒</h1> 
+//       <p className="app-subtitle">Contactos guardados</p> 
+
+//       {/* Recorremos el arreglo contactos y pintamos una tarjeta por cada uno */} 
+//       {contactos.map((c) => ( 
+//         <ContactoCard 
+//           key={c.id}            // key única para React 
+//           nombre={c.nombre}     // prop nombre 
+//           telefono={c.telefono} // prop telefono 
+//           correo={c.correo}     // prop correo 
+//           etiqueta={c.etiqueta} // prop etiqueta (Cliente, Instructor, etc.) 
+//         /> 
+//       ))} 
+//       <p className="app-nota"> 
+//         (Versión 0.1 - solo lectura, sin agregar ni editar todavía) 
+//       </p> 
+//     </main> 
+//   ); 
+// } 
+
+import { useState } from "react";;
+import "./App.css";;
+import ContactoCard from "./Componentes/ContactoCard";
+import FormularioContacto from "./Componentes/FormularioContacto";
 export default function App() {
-  const fecha = new Date().toLocaleString();
-  return (
-    <main>
-      {" "}
-      <h1>Hola SENA</h1> <p>Fecha y hora actual: {fecha}</p>{" "}
-    </main>
-  );
+ const [contactos, setContactos] = useState([
+ {
+ id: 1,
+ nombre: "Carolina Pérez",
+ telefono: "300 123 4567",
+ correo: "carolina@sena.edu.co",
+ etiqueta: "Compañera",
+ },
+ ]);
+
+ const agregarContacto = (nuevoContacto) => {
+ setContactos([...contactos, { id: Date.now(), ...nuevoContacto }]);
+ };
+// Eliminar
+ const eliminarContacto = (id) => {
+ setContactos((prev) => prev.filter((c) => c.id !== id));
+ };
+ return (
+ <main className="app-container">
+ <h1 className="app-title">Agenda ADSO v2</h1>
+
+ <FormularioContacto onAgregar={agregarContacto} />
+
+ <section className="lista-contactos">
+ {contactos.map((c) => (
+ <ContactoCard
+ key={c.id}
+ id={c.id}
+ nombre={c.nombre}
+ telefono={c.telefono}
+ correo={c.correo}
+ etiqueta={c.etiqueta}
+ onDelete={eliminarContacto}
+ />
+ ))}
+ </section>
+ </main>
+ );
 }
